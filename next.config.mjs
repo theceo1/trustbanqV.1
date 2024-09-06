fbaa3ba6e80548d2368aa34a00fc9c9e6fbc3ea5
theceo1 <loysiustony@hotmail.com.hk>
2024-09-06 06:29:36 +0100

    Add nextConfig with reactStrictMode, assetPrefix, and images settings; remove rewrites.

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
   reactStrictMode: true,
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
   images: {
-    domains: ['localhost', 'https://trustbank1.vercel.app/'],
+    domains: ['localhost', 'trustbank1.vercel.app'],
     loader: 'default',
     path: '/_next/image',
   },
