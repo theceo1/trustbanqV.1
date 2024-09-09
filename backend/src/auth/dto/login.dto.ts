384b6633f48873656c2dbd03ad92eea945237c12
theceo1 <loysiustony@hotmail.com.hk>
2024-09-09 01:55:06 +0100

    Update vercel.json paths, remove 'name' and 'googleId' from LoginDto, add MinLength.

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,15 +1,11 @@
 //backend/src/auth/dto/login.dto.ts
-import { IsEmail, IsString, IsOptional } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   email: string;
 
   @IsString()
+  @MinLength(6)
   password: string;
-
-
-  @IsOptional()
-  @IsString()
-  googleId?: string;
 }
